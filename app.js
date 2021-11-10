4e598b36c8e4a599ee5244ed148f6c989d0a0e20
Carlos Fung <carlos.fung@me.com>
2021-11-10 10:24:12 +0100

    final final

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.all("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
 app.all("/home", (request, response) => {
   response.sendFile(path.join(__dirname, "views", "home.html"));
 });
